9e5295000efcb690dbbe4012839a8b3348841ff6
Saniya pandey <273495977+pandeysaniya980-blip@users.noreply.github.com>
2026-09-16 12:57:14 +0530

    Create 05_perimeter_of_rectangle.c

---
+++ 05_perimeter_of_rectangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int l,b;
+    printf("enter side l:");
+    scanf("%d",&l);
+    printf("enter side b:");
+    scanf("%d",&b);
+
+
+    printf("perimeter is : %d",2*(l+b));
+    return 0;
+
+}
