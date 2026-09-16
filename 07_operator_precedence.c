352362ce36301afa2c29dc6cec58abe3e80ca8a4
Saniya pandey <273495977+pandeysaniya980-blip@users.noreply.github.com>
2026-09-16 19:38:59 +0530

    Create 07_operator_precedence.c

---
+++ 07_operator_precedence.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+//operator precedence(priority order)
+
+int main()
+{
+    printf("%d\n",5*2-2*3);
+    printf("%d\n",5*2/2*3);
+    printf("%d\n",5*(2/2)*3);
+    printf("%d",5+2/2*3);
+    return 0;
+}
